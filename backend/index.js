92439b5ed9d48037c9ccab04067c5d02ec49c515
AJYI <alexjinwooyi@gmail.com>
2022-09-19 23:50:32 -0700

    Implemented code for mysql db

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const dotEnv = require('dotenv');
+const cors = require('cors');
+const mysql_db_connection = require('./database/mysql_connection')
+
+dotEnv.config();
+
+const app = express();
+
+//DB connectivity
+let temp = mysql_db_connection()
+
+
+// Request payload middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// cors
+app.use(cors);
+
+app.get('/', (req, res, next) => {
+    res.send('Hello from Node Server');
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
+
+// Global error handle middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send({
+        status: 500,
+        message: err.message,
+        body: {}
+    })
+  })
