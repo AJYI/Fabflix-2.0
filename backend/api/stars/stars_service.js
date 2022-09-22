dcc098fa8bd543b92e75566809c4db1a33f43a7c
AJYI <alexjinwooyi@gmail.com>
2022-09-22 14:44:33 -0700

    Completed single star api

--- backend/api/stars/stars_service.js
+++ backend/api/stars/stars_service.js
@@ -5,6 +5,20 @@ module.exports.get_single_star = async(request_params) => {
     try {
         const star_id = request_params.star_id
         const star_entry = await db.stars.findByPk(star_id ,{
+            include: [
+                {
+                    model: db.movies,
+                    attributes: [
+                        'id', 
+                        'title',
+                        [sequelize.literal('(select rating from ratings where ratings.MovieId = movies.id)'), 'rating']
+                    ],
+                    through: {
+                        attributes: []
+                    }
+                }
+            ],
+            order: [sequelize.literal("`movies.rating` DESC")],
         });
         return star_entry;
     } catch (error) {
--- backend/database/sequelize/database.js
+++ backend/database/sequelize/database.js
@@ -6,24 +6,23 @@ const stars_in_movies = require('./models/stars_in_movies');
 const stars = require('./models/stars');
 
 
+const database_object = {}
 
-  const database_object = {}
+// M:N Associations (Junction Tables)
+movies.belongsToMany(genres, {through: genres_in_movies});
+genres.belongsToMany(movies, {through: genres_in_movies});
 
-  // M:N Associations (Junction Tables)
-  movies.belongsToMany(genres, {through: genres_in_movies});
-  genres.belongsToMany(movies, {through: genres_in_movies});
+movies.belongsToMany(stars, {through: stars_in_movies, foreignKey: 'movieId'});
+stars.belongsToMany(movies, {through: stars_in_movies, foreignKey: 'starId'});
 
-  movies.belongsToMany(stars, {through: stars_in_movies, foreignKey: 'movieId'});
-  stars.belongsToMany(movies, {through: stars_in_movies, foreignKey: 'starId'});
+// Regular associations
+movies.hasOne(ratings,{foreignKey: 'movieId'});
+ratings.belongsTo(movies);
 
-  // Regular associations
-  movies.hasOne(ratings,{foreignKey: 'movieId'});
-  ratings.belongsTo(movies);
-
-  // Adding to the obj
-  database_object.genres = genres;
-  database_object.movies = movies;
-  database_object.ratings = ratings;
-  database_object.stars = stars;
+// Adding to the obj
+database_object.genres = genres;
+database_object.movies = movies;
+database_object.ratings = ratings;
+database_object.stars = stars;
 
 module.exports = database_object;
