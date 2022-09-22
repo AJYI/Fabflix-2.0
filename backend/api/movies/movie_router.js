b8521420b8583fd3ceeda77bb8e68c9f2ed48166
AJYI <alexjinwooyi@gmail.com>
2022-09-21 20:55:38 -0700

    Implemented single movie list api

--- backend/api/movies/movie_router.js
+++ backend/api/movies/movie_router.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const movie_controller = require('./movie_controller');
 
-router.post('/create-movie-entry', movie_controller.create_movie_entry);
 router.get('/get-all-movies', movie_controller.get_all_movies);
 router.get('/get-single-movie/:movie_id', movie_controller.get_single_movie);
+router.post('/create-movie-entry', movie_controller.create_movie_entry);
 
 module.exports = router;
